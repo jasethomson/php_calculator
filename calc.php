9ec7c87ad7d9f2e1633a76bf22cb50de1c1abaab
jasethomson <jasethomson31@gmail.com>
2019-11-24 17:30:09 -0800

    completed php OOP caclulator

--- calc.php
+++ calc.php
@@ -1,7 +1,14 @@
 <?php
 
+require_once './incloud/calcInclude.php';
+
 $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
 $cal = $_POST['cal'];
+
+
+$calculator = new Calc($num1, $num2, $cal);
+
+print $calculator->calcMethod();
 
 ?>
